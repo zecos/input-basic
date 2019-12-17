5d85f0659c3636197a2c0dfd475f50e85b3da30e
Zane Hitchcox <zwhitchcox@gmail.com>
2019-12-17 00:23:07 -0500

    add slider

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { Text } from './input-creators/Text'
 export { TextArea } from './input-creators/TextArea'
 export { Select } from "./input-creators/Select"
+export { Slider } from './input-creators/Slider'

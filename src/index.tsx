556fb1370ef6e536847e86ccd34a55e11259736b
Zane Hitchcox <zwhitchcox@gmail.com>
2019-12-17 23:46:48 -0500

    radio

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,4 @@ export { TextArea } from './input-creators/TextArea'
 export { Select } from "./input-creators/Select"
 export { Slider } from './input-creators/Slider'
 export { Checkbox } from './input-creators/Checkbox'
+export { Radio } from './input-creators/Radio'
